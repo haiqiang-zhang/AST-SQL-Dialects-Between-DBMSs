1dc3191a9d2e58e4c797d40d9c712ffe3ef2e6ab
Troye Huang <hz138088@gmail.com>
2024-05-25 15:41:47 +0200

    Revert "1"
    This reverts commit 8ee72cb795c40eac5e77f028f26aa55e5358987e.

--- setup_test_split.py
+++ setup_test_split.py
@@ -2,6 +2,7 @@ import os
 from utils import clean_query, clean_query_postgresql
 
 dbms_tests = ['clickhouse', 'mysql', 'postgresql', 'sqlite', 'duckdb']
+# dbms_tests = ['postgresql']
 
 def extract_and_save_sql_statements(input_sql_file, setup_directory, dbms_test):
     # setup keywords
@@ -12,6 +13,7 @@ def extract_and_save_sql_statements(input_sql_file, setup_directory, dbms_test):
         os.makedirs(setup_directory)
     
     with open(input_sql_file, 'r', encoding='utf-8') as sql_file:
+        # lines = sql_file.readlines()
         content = sql_file.read()
     
     clean_query_list = None
@@ -38,14 +40,14 @@ def extract_and_save_sql_statements(input_sql_file, setup_directory, dbms_test):
             remaining_statements.append(q)
     
     output_txt_file = os.path.join(setup_directory, os.path.basename(input_sql_file).replace('.sql', '.txt').strip())
-    # print(output_txt_file)
-    
+    print(output_txt_file)
     with open(output_txt_file, 'w', encoding='utf-8') as txt_file:
         txt_file.writelines(extracted_statements)
     
     with open(input_sql_file, 'w', encoding='utf-8') as sql_file:
         sql_file.writelines(remaining_statements)
 
+# directory = './test_case/postgresql/test'
 for dbms_test in dbms_tests:
     test_directory = f'./test_case/{dbms_test}/test'
     setup_directory = f'./test_case/{dbms_test}/setup'
